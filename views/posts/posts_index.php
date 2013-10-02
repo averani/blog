10765f072478d783e2f41ccd4dbc21ea1f60cec9
Ave <ave.rani@khk.ee>
2013-10-02 11:41:06 +0300

    Create new view

--- views/posts/posts_index.php
+++ views/posts/posts_index.php
@@ -9,6 +9,8 @@
 <?foreach($posts as $post):?>
 <div class="span8">
 	<h1>Alice in Wonderland, part dos</h1>
+
+	
 	<p>'You ought to be ashamed of yourself for asking such a simple question,' added the Gryphon; and then they both sat silent and looked at poor Alice, who felt ready to sink into the earth. At last the Gryphon said to the Mock Turtle, 'Drive on, old fellow! Don't be all day about it!' and he went on in these words:
 		'Yes, we went to school in the sea, though you mayn't believe it—'
 		'I never said I didn't!' interrupted Alice.
@@ -26,5 +28,5 @@
 
 	<hr>
 </div>
-<?endforeach:?>
+<?endforeach;?>
 
